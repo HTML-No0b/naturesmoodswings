0185b260467cd23022cd97235adf75f363219e9f
Richard Martin <richie@Richards-MacBook-Air.local>
2022-10-10 22:25:39 -0500

    added city lon & lat

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 var url = "http://api.openweathermap.org";
 var apiKey = "731ac8af444163b219a889c601c050eb";
 var searchHistory = [];
+const searchEl = document.createElement('div')
+//default city
+let lat = 44.954445;
+let lon = -93.091301;
+
 //create vars for DOM elements refs
 //create function to display search list
 //create function to render search hisoty
     //create for loop to go through search history 
         //make button for every item in search history then append to search history
             //append buttons to search history
+//function to update history in local storage
+    //call rendersearch so it shows recent history
+//function to call history in local storage
+//function that renders the current weather (expect it to be 30-40 lines)
+//function that renders forcast card
+//function that duplicates for each day x5
+    //render items
+//function that
+function renderItems(city, data) {
+    renderCurrentWeather(city, data.list[0], data.city.timezone);
+    renderForecast(data.list);
+  };
 function fetchWeather(location) {
     var { lat } = location;
     var { lon } = location;
@@ -43,8 +60,3 @@ fetch(apiUrl)
     console.error(err)
 })
 };
-function renderItems(city, data) {
-    renderCurrentWeather(city, data.list[0], data.city.timezone);
-    renderForecast(data.list);
-  };
-  fetchCoordinates(Denver);
